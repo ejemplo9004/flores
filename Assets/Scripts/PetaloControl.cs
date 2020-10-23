812a823d606eb50df751928165526d91149b9f9c
Morion <adrian.martinez@udea.edu.co>
2020-10-23 16:28:39 -0500

    verdadero commit inicial

---
+++ Assets/Scripts/PetaloControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class PetaloControl : MonoBehaviour
+{
+    public Mesh malla1;
+    public Mesh malla2;
+    [Range(0,1)]
+    public float t;
+    public Mesh meshFinal;
+
+    private MeshRenderer miRenderer;
+    private MeshFilter miMFilter;
+    private Vector3[] v1;
+    private Vector3[] v2;
+    private Vector3[] vFinal;
+
+    private void Awake()
+    {
+        miRenderer = GetComponent<MeshRenderer>();
+        miMFilter = GetComponent<MeshFilter>();
+        v1 = malla1.vertices;
+        v2 = malla2.vertices;
+        meshFinal = new Mesh();
+        meshFinal.vertices = malla1.vertices;
+        meshFinal.triangles = malla1.triangles;
+        meshFinal.normals = malla1.normals;
+        meshFinal.uv = malla1.uv;
+        
+        meshFinal.name = "Malla generada";
+    }
+    void Start()
+    {
+        miMFilter.mesh = meshFinal;
+        ActualizarMalla();
+    }
+
+    public void CalcularVFinal()
+    {
+        v1 = malla1.vertices;
+        v2 = malla2.vertices;
+        vFinal = new Vector3[v1.Length];
+        for (int i = 0; i < v1.Length; i++)
+        {
+            vFinal[i] = Vector3.Lerp(v1[i], v2[i], t);
+        }
+    }
+
+    void ActualizarMalla()
+    {
+        CalcularVFinal();
+        meshFinal.vertices = vFinal;
+        meshFinal.RecalculateBounds();
+        meshFinal.RecalculateNormals();
+    }
+
+    void FixedUpdate()
+    {
+        ActualizarMalla();
+    }
+}
---
+++ Assets/Scripts/Pintar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pintar : MonoBehaviour
+{
+    Vector3 ultimaPosicion = Vector3.zero;
+    public GameObject brocha;
+    [Range(0.1f, 2f)]
+    public float tamaño = 0.5f;
+    public Camera camara;
+    public GameObject vistaPrevia;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray rayo = camara.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(rayo,out hit))
+            {
+                if (hit.point != ultimaPosicion)
+                {
+                    ultimaPosicion = hit.point;
+                    GameObject gi = Instantiate(brocha, hit.point, Quaternion.identity, transform) as GameObject;
+                    gi.transform.localScale = Vector3.one * tamaño/10f;
+                    gi.transform.Translate(Vector3.back * 0.001f);
+                }
+            }
+        }
+    }
+
+    public void CambiarTamaño(float t)
+    {
+        tamaño = t;
+        vistaPrevia.transform.localScale = t *Vector3.one;
+    }
+
+    public void BorrarLienzo()
+    {
+        MeshRenderer[] gos = gameObject.GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < gos.Length; i++)
+        {
+            if(gos[i].gameObject != gameObject) Destroy(gos[i].gameObject);
+        }
+    }
+}
