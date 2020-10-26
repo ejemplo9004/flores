4b2764616433c0ab3f30f6998764c91c5f99cd59
Jhonny-GaleanoBarrero <jhonny.galeanob@udea.edu.co>
2020-10-25 21:28:42 -0500

    musica

---
+++ Assets/Scripts/ControlDeEscenas.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlDeEscenas : MonoBehaviour
+{
+    public void cambiarEscena(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void salirJuego()
+    {
+        Application.Quit();
+    }
+}
