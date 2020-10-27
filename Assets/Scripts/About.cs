0d9b2348c63b27c435fa2ca50490ac589f30aac8
Morion <adrian.martinez@udea.edu.co>
2020-10-27 14:01:29 -0500

    Arreglado el menú e integración del About

---
+++ Assets/Scripts/About.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class About : MonoBehaviour
+{
+    [Multiline(3)]
+    public string[] frases;
+    public float tiempoEntreLetras=0.1f;
+    public float tiempoEntreFrases;
+    public Text txtFrases;
+    int indice;
+    private IEnumerator Start()
+    {
+        do
+        {
+            for (int i = 0; i <= frases[indice].Length; i++)
+            {
+                txtFrases.text = frases[indice].Substring(0, i);
+                if (i%2==0)
+                {
+                    txtFrases.text = txtFrases.text + "|";
+                }
+                yield return new WaitForSeconds(tiempoEntreLetras);
+            }
+
+            yield return new WaitForSeconds(tiempoEntreFrases);
+            for (int i = 0; i <= frases[indice].Length; i++)
+            {
+                txtFrases.text = frases[indice].Substring(0, frases[indice].Length - i);
+                yield return new WaitForSeconds(tiempoEntreLetras/2f);
+            }
+            indice = (indice + 1) % frases.Length;
+        } while (true);
+    }
+}
